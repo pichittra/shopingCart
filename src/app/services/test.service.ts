6636c262b6edd7be76c40b9a2bc7067278eaec3a
pichittra <pichittra.cch@gmail.com>
2017-09-13 14:53:56 +0700

    test service

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -3,22 +3,48 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class TestService {
   arr: any = []
-  nums: any;
+
   constructor() {
     this.arr = [3, 2, 'l', 5, 'g', 'c', 5] //20
     this.test(this.arr);
+    //  console.log(this.test(this.arr))
+
   }
   test(arr) {
     let sum = 0;
-    let text = '';
-    for (let i = 0; i < arr.length; i++) {
-      if(typeof arr[i] === 'number'){
-       sum += arr[i];
-      }else{
-        text = text+arr[i];
+    let text = [];
+    // let totalNum = [];
+    // let totalString = [];
+    // let total = [];
+    // let toString = [];
+    // for (let i = 0; i < arr.length; i++) {
+    //   if(typeof arr[i] === 'number'){
+    //    sum += arr[i];
+    //   }else{
+    //     text = text+arr[i];
+    //   }
+    // }
+    //  return sum+text;
+    // console.log(arr)
+    return arr.reduce(function (num, array, index) {
+
+      if (typeof array === 'number') {
+        //totalNum.push(array);
+        //console.log(sum)
+        num += array;
+        console.log(num)
+      } else {
+        text.push(array);
+        text.toString();
+         console.log(text)
+        // totalString.push(array);
+        //text = text+arr;
+        // console.log(array)
       }
-    }
-    return sum+text;
+
+      return  num+text;
+      //   console.log(index)
+    })
   }
 
 }
